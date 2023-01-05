31f41470cd96d318fd80aac7736587cf4ff30cb4
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-01-05 13:00:50 +0600

    shipping address get successfully

--- src/Components/Bderp.js
+++ src/Components/Bderp.js
@@ -16,6 +16,7 @@ const Bderp = () => {
     const [house, setHouse] = useState('');
     const [phone, setPhone] = useState('');
     const [fax, setFax] = useState('');
+    const [shippingAddress, setShippingAddress] = useState({})
 
     useEffect(() => {
         (async () => {
@@ -28,14 +29,24 @@ const Bderp = () => {
         )()
     }, [])
     // const countryName = country.toLowerCase()
-    const allData = { name, country, division, district, city, town, zip, village, house, phone, fax }
-
-    // console.log(allData)
+    
+    let newCountry = country.toString();
+    newCountry = typeof(newCountry)
+    const serchByCountry = countryData?.filter((country) => country.country === ('bangladesh' || country));
     const serchOfCountry = countryData?.find((country) => country.country === 'bangladesh');
     const serchDivision = serchOfCountry?.division?.find((division) => division.name === 'dhaka');
     const searchDistrict = serchDivision?.district?.find((district) => district.name === 'dhaka');
     const searchSubDistrict = searchDistrict?.subDistrict?.find((subdist) => subdist.name === 'uttara');
     const searchTown = searchSubDistrict?.town?.find((town) => town.name === 'nodda');
+    
+    // console.log(newCountry, country, serchByCountry)
+    
+    const handleShippingAddress = () => {
+        const allData = { name, country, division, district, city, town, zip, village, house, phone, fax };
+        setShippingAddress(allData)
+    }
+
+    console.log(shippingAddress)
 
     return (
         <div className='bg-[#F9F9F9] py-10 px-5'>
@@ -154,7 +165,122 @@ const Bderp = () => {
                         </div>
                     </form>
                 </div>
-                <div></div>
+
+                {/* shipping address starts */}
+
+                <div>
+                    <h2 className='text-[#000] text-2xl font-semibold'>Shipping Address</h2>
+                    <p className='cursor-pointer text-primary underline' onClick={handleShippingAddress}>Copy billing address</p>
+                    <form>
+                        <div className='pb-2'>
+                            <label className='block text-[#000]' htmlFor="">Attention</label>
+                            <input className='bg-[#fff] border-2 focus:outline-none pl-2 w-full' type="text" name="customername" id="name" value={shippingAddress?.name} />
+                        </div>
+                        <div className='pb-2'>
+                            <label className='block text-[#000]' htmlFor="">Country</label>
+                            <select
+                                value={shippingAddress?.country}
+                                className='w-full border-2 focus:outline-none' name="country" id="">
+                                <option value="Please search">Please search</option>
+                                {/* {
+                                    countryData.length &&
+                                    countryData.map((country, index) =>
+                                        <option className='capitalize' key={index} value={country.country}>{country.country}</option>
+                                    )
+                                } */}
+                            </select>
+                        </div>
+                        <div className='pb-2'>
+                            <label className='block text-[#000]' htmlFor="">Division/State</label>
+                            <select
+                               value={shippingAddress?.division}
+                                className='w-full border-2 focus:outline-none' name="state" id="">
+                                <option value="Please search">Please search</option>
+                                {/* {
+                                    serchOfCountry?.division?.map((division, index) =>
+                                        <option className='capitalize' key={index} value={division.name}>{division.name}</option>
+                                    )
+                                } */}
+                            </select>
+                        </div>
+                        <div className='pb-2'>
+                            <label className='block text-[#000]' htmlFor="">District</label>
+                            <select
+                                value={shippingAddress?.district}
+                                className='w-full border-2 focus:outline-none' name="district" id="">
+                                <option value="Please search">Please search</option>
+                                {/* {
+                                    serchDivision?.district?.map((district, index) => 
+                                    <option key={index} value={district.name}>{district.name}</option>
+                                    )
+                                } */}
+                            </select>
+                        </div>
+                        <div className='pb-2'>
+                            <label className='block text-[#000]' htmlFor="">City/Sub-district/Thana</label>
+                            <select
+                               value={shippingAddress?.city}
+                                className='w-full border-2 focus:outline-none' name="city" id="">
+                                <option value="Please search">Please search</option>
+                                {/* {
+                                    searchDistrict?.subDistrict?.map((subdist, index) => 
+                                    <option key={index} value={subdist.name}>{subdist.name}</option>
+                                    )
+                                } */}
+                            </select>
+                        </div>
+                        <div className='pb-2'>
+                            <label className='block text-[#000]' htmlFor="">Union/Area/Town</label>
+                            <select
+                               value={shippingAddress?.town}
+                                className='w-full border-2 focus:outline-none' name="town" id="">
+                                <option value="Please search">Please search</option>
+                                {/* {
+                                    searchSubDistrict?.town?.map((town, index) =>
+                                    <option key={index} value={town.name}>{town.name}</option>
+                                    )
+                                } */}
+                            </select>
+                        </div>
+                        <div className='pb-2'>
+                            <label className='block text-[#000]' htmlFor="">Zip Code</label>
+                            <input
+                               value={shippingAddress?.zip}
+                                className='bg-[#fff] border-2 focus:outline-none pl-2 w-full' type="text" name="zip" id="zip" />
+                        </div>
+                        <div className='pb-2'>
+                            <label className='block text-[#000]' htmlFor="">Street Address/Village</label>
+                            <select
+                              value={shippingAddress?.village}
+                                className='w-full border-2 focus:outline-none' name="street" id="">
+                                <option value="Please search">Please search</option>
+                                {/* {
+                                    searchTown?.village?.map((village, index) => 
+                                    <option key={index} value={village.name}>{village.name}</option>
+                                    )
+                                } */}
+                            </select>
+                        </div>
+                        <div className='pb-2'>
+                            <label className='block text-[#000]' htmlFor="">House Suite/Apartment</label>
+                            <input
+                              value={shippingAddress?.house}
+                                className='bg-[#fff] border-2 focus:outline-none pl-2 w-full' type="text" name="house" id="house" placeholder='house number:' />
+                        </div>
+                        <div className='pb-2'>
+                            <label className='block text-[#000]' htmlFor="">Phone</label>
+                            <input
+                               value={shippingAddress?.phone}
+                                className='bg-[#fff] border-2 focus:outline-none pl-2 w-full' type="text" name="phone" id="phone" placeholder='Phone number' />
+                        </div>
+                        <div className='pb-2'>
+                            <label className='block text-[#000]' htmlFor="">Fax</label>
+                            <input
+                             value={shippingAddress?.fax}
+                                className='bg-[#fff] border-2 focus:outline-none pl-2 w-full' type="text" name="fax" id="fax" placeholder='Fax' />
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
